2af0879e58e6927439dfcd22dae188aabd843e16
PavloPantus <mr.pantus@gmail.com>
2020-01-22 00:06:36 +0200

    Updates

---
+++ precache-manifest.d9c702be99ec6f1c7cd4d2015bd1eb3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a206d5532e506c7b4c6af14e432cdb68",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "a90d6c2e507be792b70d",
+    "url": "/react_uber-eats/static/css/main.d4b36912.chunk.css"
+  },
+  {
+    "revision": "c5b00c20a655efb0a962",
+    "url": "/react_uber-eats/static/js/2.0fb48b3f.chunk.js"
+  },
+  {
+    "revision": "a90d6c2e507be792b70d",
+    "url": "/react_uber-eats/static/js/main.079b8e8a.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
